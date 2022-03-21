1a5d3824c04a912d01de0c953680dbb8c43864df
playck <playck@naver.com>
2022-03-21 23:04:27 +0900

    [client] InputChat Component Ui

---
+++ client/src/components/ChatRoomDetail/SentMessage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SentMessage = () => {
+  return <div></div>;
+};
+
+export default SentMessage;
--- client/src/pages/RoomDetail.tsx
+++ client/src/pages/RoomDetail.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "@emotion/styled/macro";
+import { Global, css } from "@emotion/react";
+import BottomNavigation from "../components/BottomNavigation";
 
-const RoomDetail = () => {
-  return <div>톡방</div>;
+const Base = styled.div``;
+
+const Container = styled.div``;
+
+const globalStyle = css`
+  body {
+    background-color: #abc1d1;
+  }
+`;
+
+const RoomDetail: React.FC = () => {
+  return (
+    <Base>
+      <Global styles={globalStyle} />
+      <Container>{}</Container>
+
+      <BottomNavigation />
+    </Base>
+  );
 };
 
 export default RoomDetail;
