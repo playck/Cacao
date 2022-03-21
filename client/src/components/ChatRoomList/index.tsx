c2a7d8e181bc4dbf7eee4f0ef272a54c12dbce99
playck <playck@naver.com>
2022-03-21 22:31:10 +0900

    [client] ChatRoom Component UI

--- client/src/components/ChatRoomList/index.tsx
+++ client/src/components/ChatRoomList/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "@emotion/styled/macro";
 
-const index = () => {
-  return <div></div>;
+const Base = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 36px 0 64px 0;
+`;
+
+const ChatRoomList: React.FC = ({ children }) => {
+  return <Base>{children}</Base>;
 };
 
-export default index;
+export default ChatRoomList;
--- client/src/pages/RoomList.tsx
+++ client/src/pages/RoomList.tsx
@@ -2,17 +2,46 @@ import React from "react";
 import BottomNavigation from "../components/BottomNavigation";
 import TopNavigation from "../components/TopNavigation";
 import styled from "@emotion/styled";
+import ChatRoomList from "../components/ChatRoomList";
+import { useQuery } from "react-query";
+import { AxiosError, AxiosResponse } from "axios";
+import { IRoom } from "../types";
+import { fetchChatRoomList } from "../api/roomApi";
+import ChatRoom from "../components/ChatRoomList/ChatRoom";
 
-const RoomList = () => {
-  const Base = styled.div``;
+const Base = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 12px;
+  box-sizing: border-box;
+`;
 
-  const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const RoomList: React.FC = () => {
+  const { data: chatRoomListData } = useQuery<
+    AxiosResponse<Array<IRoom>>,
+    AxiosError
+  >("fetchChatRoomList", fetchChatRoomList);
 
   return (
     <Base>
       <Container>
         <TopNavigation title="채팅" />
-        {/* <ChatRoomList /> */}
+        {chatRoomListData && (
+          <ChatRoomList>
+            {chatRoomListData.data.map((chatRoom) => (
+              <ChatRoom
+                key={chatRoom.id}
+                id={chatRoom.id}
+                username={chatRoom.user.username}
+              />
+            ))}
+          </ChatRoomList>
+        )}
         <BottomNavigation />
       </Container>
     </Base>
