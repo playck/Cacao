1a86b7f8d4bc5a652d6d7060e2aeb339a447a0fb
playck <playck@naver.com>
2022-03-22 00:37:16 +0900

    [client] ReceiveMessage Component UI

--- client/src/components/ChatRoomDetail/MessageList.tsx
+++ client/src/components/ChatRoomDetail/MessageList.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+import styeld from "@emotion/styled/macro";
+
 export interface MessageType {
   sender: string;
   content: string;
   timestamp: string;
 }
+
+const Base = styeld.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0 0 48px 0;
+  width: 100%;
+  > li + li {
+    margin-top: 25px;
+  }
+`;
+
+const MessageList: React.FC = ({ children }) => {
+  return <Base>{children}</Base>;
+};
+
+export default MessageList;
--- client/src/components/ChatRoomDetail/SentMessage.tsx
+++ client/src/components/ChatRoomDetail/SentMessage.tsx
@@ -1,7 +1,42 @@
+import { useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
+import { timeStamp } from "console";
 import React from "react";
+import { MessageType } from "./MessageList";
 
-const SentMessage = () => {
-  return <div></div>;
+const Base = styled.li`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-end;
+  width: 100%;
+`;
+
+const SpeechBubble = styled.span<{ backgroundColor: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border-radius: 15px 0 15px 15px;
+  margin-right: 0;
+  margin-left: 5px;
+  padding: 13px;
+  font-size: 18px;
+`;
+
+const SentAt = styled.span`
+  font-size: 14px;
+  opacity: 0.8;
+`;
+
+interface Props extends MessageType {}
+
+const SentMessage: React.FC<Props> = ({ content, timestamp }) => {
+  const theme = useTheme();
+  return (
+    <Base>
+      <SpeechBubble backgroundColor={theme.colors.primary}>
+        {content}
+      </SpeechBubble>
+      <SentAt>{timestamp}</SentAt>
+    </Base>
+  );
 };
 
 export default SentMessage;
